4b98d772facfa08ecad7bd284629f3b72445bec4
sarkis92 <haralampiev9210@gmail.com>
2015-05-23 18:42:24 +0300

    Binary Search. Input array on line 1 and searched number on a new line and the program outputs the index int the array of the searched number or -1

---
+++ BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int searchedNum = int.Parse(Console.ReadLine());
+            int midIndex;
+            int lowerIndex = 0, HighIndex = nums.Length - 1;
+
+
+            while (true)
+            {
+                midIndex = (HighIndex + lowerIndex) / 2;
+
+                if ((lowerIndex == midIndex) && (nums[midIndex + 1] == searchedNum))
+                {
+                    Console.WriteLine(midIndex + 1);
+                    break;
+                }
+
+                if (nums[midIndex] == searchedNum)
+                {
+                    Console.WriteLine(midIndex);
+                    break;
+                }
+                else if (nums[midIndex] < searchedNum)
+                {
+                    lowerIndex = midIndex;
+                }
+                else
+                {
+                    HighIndex = midIndex;
+                }
+
+                if (HighIndex == lowerIndex)
+                {
+                    Console.WriteLine(-1);
+                    break;
+                }
+            }
+        }
+    }
+}
