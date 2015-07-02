e31e8f3f9dee6d58d5c5a41bb3a33fd2fbb0420e
sarkis92 <haralampiev9210@gmail.com>
2015-07-02 16:59:36 +0300

    CSV Parser

---
+++ CSV Parser/CSV Parser/CSVParser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+namespace CSV_Parser
+{
+    public static class CSVParser
+    {
+        public static void Parser(string line)
+        {
+            int counter = 0;
+            while (counter < line.Length)
+            {
+                switch (line[counter])
+                {
+                    case ',':
+                        break;
+
+                    case '"':
+                        StringBuilder quotedToken = new StringBuilder();
+                        quotedToken.Append(line[counter]);
+                        counter++;
+                        
+                        while (line[counter] != '"')
+                        {
+                            quotedToken.Append(line[counter]);
+                            counter++;
+                        }
+                        quotedToken.Append(line[counter]);
+                        Console.WriteLine(quotedToken);
+                        break;
+                        
+                    default:
+                        StringBuilder token = new StringBuilder();
+                        while (line[counter] != ',')
+                        {
+                            token.Append(line[counter]);
+                            counter++;
+
+                            if (counter >= line.Length)
+                                break;
+                        }
+                        Console.WriteLine(token);
+                        break;
+                }
+                counter++;
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ CSV Parser/CSV Parser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace CSV_Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("test.txt");
+
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                CSVParser.Parser(line);
+                line = sr.ReadLine();
+            }
+        }
+    }
+}
